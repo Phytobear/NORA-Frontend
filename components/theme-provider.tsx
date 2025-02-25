0e5d13c98d0cbff5f52188fc0fb298c30802c6dc
Phytobear <willoh91@gmail.com>
2025-02-25 22:33:01 +0000

    i cant remember but it works

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,6 +2,7 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
 
+//fixed now? find out why this is not working?
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
